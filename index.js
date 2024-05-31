5d65ce56cf83d283e6ccc8238afab927e263ca8d
hcnhuquynh <na.nhuquynh1304@gmail.com>
2024-05-31 18:27:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Discord.Client({
     ],
 });
 //Start the Bot
-client.login(config.token)
+client.login(process.env.token)
 
 //we gotta adjust this, so we only require the file once the bot is ready!
 client.on("ready", () => {
